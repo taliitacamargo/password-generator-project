3dc9b7a5d4cd78231b0ea63cb411258b25fe7509
Tali <taliitacamargo@icloud.com>
2021-09-16 15:34:54 -0400

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # password-generator-project
+Password generator created using javascript. 
+Deployed URL:                           GitHub Repo URL:
+https://github.com/taliitacamargo/password-generator-project/issues/1#issue-998556477
---
+++ assets/script.js
@@ -0,0 +1,19 @@
+
+
+function genPassword() {
+var password = document.getElementById ("password");
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 8;
+var password = "";
+for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+}
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");
+}
